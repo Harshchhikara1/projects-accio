d76b7fab3652887c2866f14063502a66d6f407d1
Harshchhikara1 <79436865+Harshchhikara1@users.noreply.github.com>
2022-10-08 22:03:59 +0530

    Update main(brick).java

--- main(brick).java
+++ main(brick).java
@@ -1,28 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Demogame;
+
+
+
 
 import javax.swing.JFrame;
 
-/**
- *
- * @author LENOVO
- */
-public class MainClass {
- 
-    public static void main(String[] args){
-        JFrame f = new JFrame();
-        f.setTitle("Brick Breaker");
-        f.setSize(700,600);
-        f.setLocationRelativeTo(null);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setVisible(true);
-        f.setResizable(false);
-        
-        Gameplay gameplay= new Gameplay();
-         f.add(gameplay);
-        
-    }
+public class Mainclass {
+
+	 public static void main(String[] args) {
+	        JFrame obj = new JFrame();
+	        GamePlay gameplay = new GamePlay();
+	        obj.setBounds(10,10,700,600);
+	        obj.setTitle("BrickBreaker");
+	        obj.setResizable(false);
+	        obj.setVisible(true);
+	        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        obj.add(gameplay);
+	    }
 }
